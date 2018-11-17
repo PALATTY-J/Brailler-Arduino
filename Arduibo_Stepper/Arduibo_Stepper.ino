9af974061157a2052992cae9df35a3e5a72d5dee
JESVIN PALATTY <jesvinpalatty@gmail.com>
2018-11-18 00:19:22 +0530

    Tried UART Control

--- Arduibo_Stepper/Arduibo_Stepper.ino
+++ Arduibo_Stepper/Arduibo_Stepper.ino
@@ -1,7 +1,11 @@
 const int stepPinX = 8; 
 const int dirPinX = 9; 
-const int stepPinY = 10; 
-const int dirPinY = 11;
+const int stepPinY = A0; 
+const int dirPinY = A1;
+
+
+const int RX = 10; 
+const int TX = 11;
 
 const int LlimitSwitch = 2;
 const int LlimitLED=4;
@@ -11,6 +15,14 @@ const int RlimitLED=5;
 
 const int solenoidIP1 = 7;
 const int solenoidIP2 = 6;
+
+const int ackPin= A5;
+
+#include <SoftwareSerial.h>
+#include <stdlib.h>
+
+
+SoftwareSerial mySerial(RX, TX); // RX, TX
 
 
  
@@ -31,30 +43,85 @@ void setup()
   pinMode(RlimitLED,OUTPUT);
   pinMode(RlimitSwitch,INPUT);
 
+  pinMode(ackPin,OUTPUT);
+
 
 
   digitalWrite(dirPinX,LOW);
   digitalWrite(dirPinY,LOW);
 
+  digitalWrite(ackPin,HIGH);
+
+
   attachInterrupt(digitalPinToInterrupt(RlimitSwitch),resetPosition,LOW);
   initialCalibration();
+  mySerial.begin(9600);
+   Serial.begin(9600);
   
 
  
 }
-void loop() {
-  
-  
+void loop()
+{
 
-  for(int x=0;x<=100;x++)
+//  for (int x=0;x<=500;x++)
+//  {
+//    digitalWrite(stepPinY,HIGH); 
+//    delayMicroseconds(730); 
+//    digitalWrite(stepPinY,LOW); 
+//    delayMicroseconds(730);
+//  }
+//
+//  for (int x=0;x<=500;x++)
+//  {
+//    digitalWrite(stepPinX,HIGH); 
+//    delayMicroseconds(500); 
+//    digitalWrite(stepPinX,LOW); 
+//    delayMicroseconds(500);
+//  }
+
+  
+   char num[2];
+
+  if(mySerial.available()>0)
+  {
+    for(int i= 0 ;i<3;i++)
     {
-
-    digitalWrite(stepPinX,HIGH); 
-    delayMicroseconds(500); 
-    digitalWrite(stepPinX,LOW); 
-    delayMicroseconds(500);
-
+      num[i] = mySerial.read();
+      delay(1);
     }
+
+    int tensPlace=dataConversion(num[1]);
+    int unitsPlace=dataConversion(num[2]);
+
+    
+
+    int toSend=(tensPlace*10)+(unitsPlace*0);
+    
+    
+    char o=num[0];
+    switch (o)
+    {
+      case 'x':
+      Xstepper(toSend);
+      break;
+
+      case 'y':
+      Ystepper(toSend);
+      break;
+
+      case 'z':
+      Zsolenoid();
+      break;
+    }
+   
+   
+    
+      
+  
+}
+
+  
 
  }
 
@@ -106,3 +173,68 @@ void resetPosition()
 
 
 }
+
+void Xstepper (int rounds)
+{
+  for (int x=0;x<=(3*rounds);x++)
+  {
+    digitalWrite(stepPinX,HIGH); 
+    delayMicroseconds(500); 
+    digitalWrite(stepPinX,LOW); 
+    delayMicroseconds(500);
+    
+  }
+  delay(100);
+  sendACK();
+  
+  
+}
+
+void Ystepper (int rounds)
+{
+    digitalWrite(dirPinY,LOW);
+
+  for (int x=0;x<=(18*rounds);x++)
+  {
+    digitalWrite(stepPinY,HIGH); 
+    delayMicroseconds(730); 
+    digitalWrite(stepPinY,LOW); 
+    delayMicroseconds(730);
+    
+  }
+  delay(100);
+
+  sendACK();
+}
+
+void Zsolenoid()
+{
+  sendACK();
+  return;
+}
+
+
+int dataConversion(char XYZ)
+{
+   
+    byte receivedByte=XYZ;
+    byte corrected=receivedByte-B110000;
+    
+
+    int correctedData=corrected;
+    return correctedData;
+    
+}
+
+void sendACK()
+{
+  digitalWrite(ackPin,LOW);
+  delayMicroseconds(25);
+  digitalWrite(ackPin,HIGH);
+  
+  
+
+  
+ 
+}
+  
